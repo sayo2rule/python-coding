303b66c922a9af8b9a8ecc0298eb1d2403219579
Ogundipe Folasayo <sayo2rule@yahoo.com>
2026-01-28 15:00:03 +0000

    updated the treasure-island.py

--- projects/06-treasure-island.py
+++ projects/06-treasure-island.py
@@ -3,7 +3,7 @@ print("Your mission is to find the treasure.")
 
 choice1 = input('You\'re at a crossroad. W here do you want to go? Type "left" or "right" ').lower()
 if choice1 == "left":
-    
+
     choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. ').lower()
     if choice2 == "wait":
         
